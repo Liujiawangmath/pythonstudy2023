8076531b8d13d9f4bf7a4fa3f1bc0db5124b4d7f
Liujiawangmath <15674760037@163.com>
2023-07-26 21:40:13 +0800

    刘嘉旺一维二维抛物代码

---
+++ pythonstudy2023/geralt/python-learn/for循环的嵌套应用.py
@@ -0,0 +1,10 @@
+"""
+坚持表白一百天，每天送十束花
+"""
+i = 0
+for i in range(100):
+    print(f"今天是向xxx表白的第{i+1}天")
+    for j in range(10):
+        print(f"送给xxx的第{j+1}朵玫瑰花")
+    print(f"xxx，我喜欢你（第{i+1}天表白结束）")
+print(f"第{i+1}天，表白成功")
